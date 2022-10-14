9688f6c8a6f860fa4674099b7b17b895d2fd071d
Robert Jones <jones.robertbrendan@gmail.com>
2022-10-14 12:31:24 -0500

    'Updated Slider'

--- src/Data.js
+++ src/Data.js
@@ -1,11 +1,14 @@
 export const slideImages = [
     {
-        img: './../images/img1.jpg'
+        img: './../images/img1.png',
+        desc: 'Rated #1 Cooler'
     },
-    {
-        img: './../images/img2.jpg'
+    {   
+        // bg: "background-color: '#444444'",
+        // img: './../images/img2.png',
+        // desc: 'Your dream setup'
     },
-    {
-        img: './../images/img3.jpg'
-    }
+    // {
+    //     img: './../images/img3.jpg'
+    // }
 ]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import HomeCards from '../components/HomeCards'
 import Navbar from '../components/Navbar'
 import NavbarTwo from '../components/NavbarTwo'
 import Slider from '../components/Slider'
@@ -9,6 +10,7 @@ const Home = () => {
         <Navbar />
         <NavbarTwo />
         <Slider />
+        {/* <HomeCards /> */}
     </div>
   )
 }
