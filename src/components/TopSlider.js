1510bb66bac89ebd1b02406c5faf0a5a0c8954d9
Robert Jones <jones.robertbrendan@gmail.com>
2022-10-19 01:08:58 -0500

    'Uploaded slider package'

---
+++ src/components/TopSlider.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Slide } from 'react-slideshow-image'
+import { slideImages } from '../Data'
+import { RiArrowLeftSLine, RiArrowRightSLine} from 'react-icons/ri'
+import 'react-slideshow-image/dist/styles.css'
+import './TopSlider.css'
+
+// Slider Styles
+
+const buttonStyle = {
+    width: "50px",
+    height: '50px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: 'none',
+    border: '0px',
+    backgroundColor: '#fff',
+    borderRadius: '50px',
+    margin: '0 2%',
+    fontSize: '2rem',
+};
+
+const properties = {
+    prevArrow: <button style={{ ...buttonStyle }}><RiArrowLeftSLine /></button>,
+    nextArrow: <button style={{ ...buttonStyle }}><RiArrowRightSLine /></button>
+}
+
+const TopSlider = () => {
+  return (
+    <div className="slide-container" style={{backgroundColor: '#444444', width: '100vw', height: '65vh', textAlign: 'center'}} >
+        <Slide {...properties} >
+
+                {slideImages.map((slideImage, index)=> (
+                <div className="each-slide" key={index}>
+                    <div style={{'backgroundImage': `url(${slideImage.image})`}}>
+                    <span>{slideImage.caption}</span>
+                    </div>
+                </div>
+                ))} 
+
+        </Slide>
+    </div>
+  )
+}
+
+export default TopSlider
