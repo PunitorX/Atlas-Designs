a3371c14be5776f966cd1d46ca44041ba07b9595
Robert Jones <JonesBrendanDev@gmail.com>
2023-01-05 15:14:56 -0600

    'Deleted slider files'

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import NavbarTwo from '../components/NavbarTwo'
-import TopSlider from '../components/TopSlider'
-import Slider from '../components/Slider'
-import HomeCards from '../components/HomeCards'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <NavbarTwo />
-        <TopSlider />
-        {/* <Slider /> */}
-        {/* <HomeCards /> */}
     </div>
   )
 }
