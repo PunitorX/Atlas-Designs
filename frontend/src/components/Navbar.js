4558d7dddcb54e767550ceb84c2608ec8ccfba4b
Robert Jones <jones.brendan1996@gmail.com>
2023-01-06 02:23:55 -0600

    'updated navbar; created searchbar'

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,59 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import SearchBar from './SearchBar'
 import { MdKeyboardArrowDown } from 'react-icons/md'
 import { AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai'
 
 const Nav = styled.nav`
-
+  height: 12vh;
+  background-color: #395B64;
 `
 
 const NavContainer = styled.div`
 
 `
 
-const Navbar = () => {
+const NavLogo = styled.div`
+
+`
+
+const Search = styled.div`
+
+`
+
+const NavList = styled.ul`
+
+`
+
+const NavItem = styled.li`
+`
+
+const NavLink = styled(Link)`
+
+`
+
+const Navbar = ({itemOne}) => {
   return (
     <Nav>
       <NavContainer>
-        Hello
+
+        <NavLogo>
+          AD
+        </NavLogo>
+
+        <Search>
+          <SearchBar />
+        </Search>
+
+        <NavList>
+          <NavItem>
+            <NavLink>
+              {itemOne}
+            </NavLink>
+          </NavItem>
+        </NavList>
+
       </NavContainer>
     </Nav>
   )
---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function SearchBar() {
+  return (
+    <Search>
+      <SearchInput />
+    </Search>
+  )
+}
+
+export default SearchBar
