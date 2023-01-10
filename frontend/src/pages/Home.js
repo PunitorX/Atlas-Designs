b0d04fb69bcccfa1391bfefc41c8634203b04af9
Robert Jones <jones.brendan1996@gmail.com>
2023-01-10 03:59:17 -0600

    'created home body; card folder created'

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from 'react'
-// import Navbar from '../components/Navbar'
-// import Footer from '../components/Footer'
+import styled from 'styled-components'
+import HomeCard from '../components/Cards/HomeCard'
+import { productArray } from '../Data/ProductData'
+
+const Body = styled.body`
+  height: 100vh;
+`
+
+const BodyContainer = styled.div`
+  margin: 0 5rem;
+  padding: 0 2rem;
+`
 
 const Home = () => {
   return (
-    <></>
+    <Body>
+      <BodyContainer>
+        Hello
+      </BodyContainer>
+    </Body>
   )
 }
 
