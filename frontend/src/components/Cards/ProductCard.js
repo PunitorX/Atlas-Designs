62fe962e30e4a9b29603fd8c9a25f8fb19e443ee
Robert Jones <jones.brendan1996@gmail.com>
2023-01-10 07:31:10 -0600

    'Updated cards'

--- frontend/src/components/Cards/ProductCard.js
+++ frontend/src/components/Cards/ProductCard.js
@@ -1,9 +1,56 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Card = styled.div`
+  margin: .5rem 0;
+  height: 250px;
+  width: 450px;
+  background-color: #2C3333;
+  border-radius: 10px;
+`
+
+const CardContainer = styled.div`
+  color: #fff;
+`
+
+const ProductContainer = styled.div`
+
+`
+
+const ProductName = styled.div`
+
+`
+
+const ProductRating = styled.div`
+
+`
+
+const ProductPrice = styled.div`
+
+`
+
+const CardImage = styled.div`
+
+`
+
+const Image = styled.img`
+
+`
+
 function ProductCard() {
   return (
-    <div>ProductCard</div>
+    <Card>
+      <CardContainer>
+        <ProductContainer>
+          <ProductName></ProductName>
+          <ProductRating></ProductRating>
+          <ProductPrice></ProductPrice>
+        </ProductContainer>
+        <CardImage>
+          <Image />
+        </CardImage>
+      </CardContainer>   
+    </Card>
   )
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,5 @@
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import styled from 'styled-components'
 import ProductCard from '../components/Cards/ProductCard'
@@ -5,36 +7,76 @@ import { productArray } from '../Data/ProductData'
 
 const Body = styled.body`
   height: auto;
+  margin: 0 2rem;
+  padding: 1rem;
 `
 
 const BodyContainer = styled.div`
-  margin: 0 5rem;
-  padding: 0 2rem;
+  
 `
 
 const BodySection = styled.section`
   height: 100vh;
 `
 
-const SectionHeader = styled.h2`
+const SectionHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const SectionTitle = styled.h2`
   color: #2C3333;
   font-family: 'Roboto Slab', sans-serif;
   font-size: 1.8rem;
   text-transform: uppercase;
+  letter-spacing: 1px;
+`
+const SectionView = styled.a`
+  cursor: pointer;
+  letter-spacing: .5px;
+  font-family: 'Montserrat', sans-serif;
+
+  &:hover {
+    transition: all .3s ease-in;
+    color: #EDA920;
+  }
+`
+
+const SectionCard = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
 `
 
 const Home = () => {
   return (
     <Body>
       <BodyContainer>
+
         <BodySection>
-          <SectionHeader>Our Best Deals</SectionHeader>
+          <SectionHeader>
+            <SectionTitle>Trending Parts</SectionTitle>
+            <SectionView>
+              View More
+              <FontAwesomeIcon icon={faAngleRight} size={'xs'} transform={'down-2, right-2'}/>
+            </SectionView>
+          </SectionHeader>
+          <SectionCard>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </SectionCard>
         </BodySection>
+
         <BodySection>
           <SectionHeader>Recent Builds</SectionHeader>
         </BodySection>
         <BodySection>
-          <SectionHeader>Trending Parts</SectionHeader>
+          <SectionHeader>Our Best Deals</SectionHeader>
         </BodySection>
       </BodyContainer>
     </Body>
