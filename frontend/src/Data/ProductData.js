f5ce5f6912ce95f6e6febc05eb81ff1f17c329a8
Robert Jones <jones.brendan1996@gmail.com>
2023-01-17 14:58:08 -0600

    'Uploaded images; added to ProductData'

--- frontend/src/Data/ProductData.js
+++ frontend/src/Data/ProductData.js
@@ -1,18 +1,32 @@
+import imgOne from '../Images/3070 ti.jpg'
+import imgTwo from '../Images/CorsairFans.jpg'
+import imgThree from '../Images/Motherboard.jpg'
+import imgFour from '../Images/RyzenFive.jpg'
+
 const productArray = [
   {
     id: '1',
-    price: 4.99,
-    item: 'GPU'
+    price: 500.00,
+    item: 'EVGA GeForce RTX 3070 Ti FTW3 Ultra Gaming Video Card',
+    img: imgOne
   },
   {
     id: '2',
-    price: 10.99,
-    item: 'CPU'
+    price: 139.00,
+    item: 'AMD Ryzen 5 5600G - Ryzen 5 5000 G-Series Cezanne 6-Core 3.9 GHz AM$ 65W AMD Radeon Graphics Desktop Processor',
+    img: imgTwo
   },
   {
     id: '3',
-    price: 20.99,
-    item: 'Motherboard'
+    price: 189.00,
+    item: 'ASUS ROG STRIX B550-F GAMING AM$ AMD B550 SATA 6Gb/s ATX AMD Motherboard',
+    img: imgThree
+  },
+  {
+    id: '4',
+    price: 89.00,
+    item: 'Corsair LL Series CO-9050072-WW LL120 RGB, 120mm Dual Light Loop RGB LED PWM Fan, 3 Fan Pack with Lighting Node PRO',
+    img: imgFour
   }
 ]
 
