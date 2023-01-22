cb2f90972eadb6f534c9efeac1cb14036313ee96
Robert Jones <JonesBrendanDev@gmail.com>
2023-01-22 01:27:20 -0600

    'Updated and styled Footer'

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+import { color } from '../GlobalColors'
+
+const FooterContainer = styled.footer`
+  height: 45vh;
+  width: auto;
+  background-color: ${color.ColorFour};
+`
+
+const FooterContain = styled.div`
+  height: inherit;
+  width: auto;
+  padding: 0 2rem;
+  margin: 0 2rem;
+`
 
 function Footer() {
   return (
     <FooterContainer>
-      
+      <FooterContain>
+
+      </FooterContain>
     </FooterContainer>
   )
 }
