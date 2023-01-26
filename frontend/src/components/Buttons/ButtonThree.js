22ebf4f215c6768c6e1303cddef77fcc2a2c2ff5
Robert Jones <jones.brendan1996@gmail.com>
2023-01-26 00:45:14 -0600

    'Created ButtonThree'

--- frontend/src/components/Buttons/ButtonThree.js
+++ frontend/src/components/Buttons/ButtonThree.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
-function ButtonThree() {
-  return (
-    <div>Explore</div>
-  )
-}
+const ButtonContainer = styled(Link)`
+    font-family: 'Lato', sans-serif;
+    cursor: pointer;
+    letter-spacing: 1px;
+    font-weight: 600;
+    
+    display: inline-block;
+    overflow: hidden;
+    color: #DFF6FF;
+    background-color: #005de0;
+    border-color: transparent;
+    border: 2px solid;
+    border-radius: 5px;
+    font-size: 16px;
+    font-weight: 700;
+    padding: 1rem;
+    border-style: solid;
+    transition: all 0.3s ease 0s;
+    text-decoration: none;
+    z-index: 1;
+    position: relative;
+    
+    &:after {
+      position: absolute;
+      content: "";
+      z-index: -1;
+      display: block;
+      left: -20%;
+      right: -20%;
+      top: -4%;
+      height: 150%;
+      width: 150%;
+      bottom: 0;
+      background: #010C2A;
+      transform: skewX(45deg) scale(0, 1);
+      transition: all 0.5s ease 0s;
+    }
+    &:focus, &:focus:active {
+      box-shadow: none;
+    }
+    &:hover {
+      box-shadow: none;
+      color: #DFF6FF;
+    }
+    &:hover:after {
+      transform: skewX(45deg) scale(1, 1);
+    }
+`
+
+function ButtonThree({children, onClick, to, as, type}) {
+    return (
+      <ButtonContainer onClick={onClick} to={to} as={as} type={type}>
+          {children}
+      </ButtonContainer>
+    )
+  }
 
 export default ButtonThree
