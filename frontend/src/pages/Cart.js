de80ebc912074d2b541064b0f92bd6548cb47b3b
Robert Jones <jones.brendan1996@gmail.com>
2023-01-28 20:48:07 -0600

    'Updated font'

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -12,17 +12,31 @@ const CartContainer = styled.div`
 const CartContent = styled.div`
   height: inherit;
   width: 100%;
+  font-family: ${font.FontTwo};
   color: ${color.ColorTwo};
   display: grid;
   grid-template-columns: 2fr 1fr;
-  gap: 15px;
+  /* gap: 15px; */
 `
+
+// Cart Section Styles
 
 const CartSection = styled.section`
   background-color: ${color.ColorThree};
+  padding: 0 1.5rem;
   height: 550px;
-  width: 100%;
+  width: 850px;
 `
+
+const SectionHeader = styled.h2`
+  padding: .5rem 0;
+  letter-spacing: 1.2px;
+  border-bottom: 1px solid ${color.ColorTwo};
+`
+
+// End Cart Section Styles
+
+// Cart Checkout Styles
 
 const CartCheckout = styled.section`
   background: ${color.ColorFour};
@@ -35,7 +49,9 @@ export const Cart = () => {
     <CartContainer>
       <CartContent>
         <CartSection>
-
+          <SectionHeader>
+            Shopping Cart
+          </SectionHeader>
         </CartSection>
         <CartCheckout>
 
